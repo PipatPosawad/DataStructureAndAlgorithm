d8202466c72c4f2b0a341d718fda14894d2601f6
Pipat Posawad <Pipat.P@draycir.com>
2024-03-28 17:37:19 +0700

    add new project

---
+++ Array/FindMinimumValue/Program.cs
@@ -0,0 +1,17 @@
+﻿using Common.Array;
+
+static int FindMinimumValue(int[] array)
+{
+    var min = array[0];
+    for (var i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+            min = array[i];
+    }
+    return min;
+}
+
+var array = new int[] { 3, 2, 4, 7, 10, 6, 5 };
+array.PrintArray();
+var result = FindMinimumValue(array);
+Console.WriteLine(result);
