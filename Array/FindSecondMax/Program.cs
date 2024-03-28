abfb8a9763e41310fa0c61219dfacd2582c53543
Pipat Posawad <Pipat.P@draycir.com>
2024-03-28 19:52:31 +0700

    add new project

---
+++ Array/FindSecondMax/Program.cs
@@ -0,0 +1,26 @@
+﻿using Common.Array;
+
+static int FindSecondMaximumValue(int[] array)
+{
+    int max = int.MinValue;
+    int secondMax = int.MinValue;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            secondMax = max;
+            max = array[i];
+        }
+        else if (array[i] > secondMax && array[i] != max)
+        {
+            secondMax = array[i];
+        }
+    }
+    return secondMax;
+}
+
+var array = new int[] { 13, 34, 2, 34, 33, 1 };
+array.PrintArray();
+var result = FindSecondMaximumValue(array);
+Console.WriteLine(result);
