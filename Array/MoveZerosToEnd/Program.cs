fb3bcc049ae401af1264a9bc91d9732427e3c984
Pipat Posawad <Pipat.P@draycir.com>
2024-03-28 19:57:59 +0700

    add new project

---
+++ Array/MoveZerosToEnd/Program.cs
@@ -0,0 +1,27 @@
+﻿using Common.Array;
+
+static int[] MoveZerosToEnd(int[] array, int n)
+{
+    int zeroIndex = 0;
+    for (int index = 0; index < n; index++)
+    {
+        if (array[index] != 0 && array[zeroIndex] == 0)
+        {
+            int temp = array[index];
+            array[index] = array[zeroIndex];
+            array[zeroIndex] = temp;
+        }
+
+        if (array[zeroIndex] != 0)
+        {
+            zeroIndex++;
+        }
+    }
+    return array;
+}
+
+var array = new int[] { 8, 1, 0, 2, 1, 0, 3 };
+array.PrintArray();
+var result = MoveZerosToEnd(array, array.Length);
+result.PrintArray();
+
