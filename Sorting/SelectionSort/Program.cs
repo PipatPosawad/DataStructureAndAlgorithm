352da6ffc82c0d3d5c1f4a0f1b92d50d38700bc3
Pipat Posawad <evolution31@hotmail.com>
2024-04-13 21:59:26 +0700

    selection sort

---
+++ Sorting/SelectionSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using Common.Array;
+
+static int[] SelectionSort(int[] array)
+{
+    for (var i = 0; i < array.Length - 1; i++)
+    {
+        var minIndex = i;
+        for (var j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minIndex])
+                minIndex = j;
+        }
+
+        // swap
+        var temp = array[minIndex];
+        array[minIndex] = array[i];
+        array[i] = temp;
+    }
+    return array;
+}
+
+var array = new int[] { 2, 11, 5, 10, 7, 8 };
+array.PrintArray();
+var result = SelectionSort(array);
+result.PrintArray();
+
