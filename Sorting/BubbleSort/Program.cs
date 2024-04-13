347bad0b74d667403d35010fe1bd26c954d60c25
Pipat Posawad <evolution31@hotmail.com>
2024-04-13 22:48:53 +0700

    bubble sort

---
+++ Sorting/BubbleSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using Common.Array;
+
+static int[] BubbleSort(int[] array)
+{
+    for (var i = 0; i < array.Length - i; i++)
+    {
+        var j = i;
+        while (j + 1 < array.Length)
+        {
+            if (array[j] > array[j + 1])
+            {
+                var temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+            j++;
+        }
+    }
+    return array;
+}
+
+var array = new int[] { 2, 11, 5, 10, 7, 8 };
+array.PrintArray();
+var result = BubbleSort(array);
+result.PrintArray();
+
