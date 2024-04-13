7015e0ba9f852111fb0da108a1aa87658bdb703c
Pipat Posawad <evolution31@hotmail.com>
2024-04-13 22:29:49 +0700

    insertion sort

---
+++ Sorting/InsertionSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using Common.Array;
+
+static int[] InsertionSort(int[] array)
+{
+    for (var i = 1; i < array.Length; i++)
+    {
+        var j = i;
+        while (j >= 1 && array[j - 1] > array[j])
+        {
+            var temp = array[j-1];
+            array[j-1] = array[j];
+            array[j] = temp;
+
+            j--;
+        }
+    }
+    return array;
+}
+
+var array = new int[] { 2, 11, 5, 10, 7, 8 };
+array.PrintArray();
+var result = InsertionSort(array);
+result.PrintArray();
+
